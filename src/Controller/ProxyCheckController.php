03b4e279bd19e5590ee719f5f51866a0b0cd4803
Maksims Alekseicevs <maksims.alekseicev@gmail.com>
2021-10-24 21:05:36 +0300

    Added function descriptions

--- src/Controller/ProxyCheckController.php
+++ src/Controller/ProxyCheckController.php
@@ -22,7 +22,10 @@ class ProxyCheckController extends AbstractController
         $this->httpClient = $httpClient;
     }
 
+
     /**
+     * Check Ip, Proxy, Tor
+     *
      * @Route("/ipcheck", name="proxy_check")
      */
     public function index(Request $request): Response
@@ -35,14 +38,14 @@ class ProxyCheckController extends AbstractController
             'REMOTE_ADDR', 'REMOTE_PORT'
         ];
 
-        $headerData[]   = '----------------------- Header Data -----------------------';
-        $headers        = $request->headers->all();
+        $headerData[] = '----------------------- Header Data -----------------------';
+        $headers = $request->headers->all();
 
-        foreach ( $headers as $key => $headersArray ) {
+        foreach ($headers as $key => $headersArray) {
 
             $value = '';
-            if ( is_array($headersArray) ) {
-                foreach ( $headersArray as $headersData) {
+            if (is_array($headersArray)) {
+                foreach ($headersArray as $headersData) {
                     $value .= ' ' . $headersData;
                 }
             }
@@ -50,33 +53,36 @@ class ProxyCheckController extends AbstractController
         }
 
 
-        $dataBrowser    = [];
-        $dataIp         = [];
-        $server         = [];
-        foreach ( $_SERVER as $key => $value ) {
+        $dataBrowser = [];
+        $dataIp = [];
+        $server = [];
+        foreach ($_SERVER as $key => $value) {
             $server[$key] = $value;
         }
 
-        foreach ( $twigParameters as $twigParameter ) {
-            if ( array_key_exists($twigParameter, $server)) {
+        foreach ($twigParameters as $twigParameter) {
+            if (array_key_exists($twigParameter, $server)) {
                 $dataBrowser[$twigParameter] = $server[$twigParameter];
             }
         }
 
-        foreach ( $ipParameters as $ipParameter ) {
-            if ( array_key_exists($ipParameter, $server)) {
+        foreach ($ipParameters as $ipParameter) {
+            if (array_key_exists($ipParameter, $server)) {
                 $dataIp[$ipParameter] = $server[$ipParameter];
             }
         }
 
         return $this->render('proxy.html.twig', [
-            'data'  => $server,
-            'ip'    => $dataIp,
+            'data' => $server,
+            'ip' => $dataIp,
         ]);
         return $this->json($data);
     }
 
+
     /**
+     * Get Tor active IP list and save to DB
+     *
      * @Route("/toriplist", name="tor_ip_list")
      */
     public function toriplist(Request $request): Response
@@ -88,13 +94,15 @@ class ProxyCheckController extends AbstractController
 
         $statusCode = $response->getStatusCode();
 
-        if ( 200 == $statusCode ) {
+        if (200 == $statusCode) {
             $content = $response->getContent();
+            $ipList = $this->validateIp($content);
         }
 
 
         return $this->json($content);
     }
+
 
     /**
      * Explore text by new line and Validate Ip by ipv4 or ipv6
@@ -103,17 +111,16 @@ class ProxyCheckController extends AbstractController
      * @return array
      */
 
-
-    public function validateIp(string $incomingText):array
+    public function validateIp(string $incomingText): array
     {
         $rowRecords = explode(PHP_EOL, $incomingText);
-        $ipRecords  = [];
+        $ipRecords = [];
 
-        foreach ( $rowRecords as $rowRecord ) {
-            if ( filter_var($rowRecord, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 )) {
+        foreach ($rowRecords as $rowRecord) {
+            if (filter_var($rowRecord, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
                 $ipRecords[] = $rowRecord;
             }
-            if ( filter_var($rowRecord, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6 )) {
+            if (filter_var($rowRecord, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
                 $ipRecords[] = $rowRecord;
             }
         }
@@ -130,18 +137,26 @@ class ProxyCheckController extends AbstractController
         return $ipRecords;
     }
 
+
+    /**
+     * Check if proxy is alive just opening connection to Ip:port (Before use)
+     *
+     * @param string $ip
+     * @param string $port
+     * @param int $errorCode
+     * @param string $errorDescription
+     * @return bool
+     */
+
     private function checkProxyAlive(string $ip, string $port, int $errorCode, string $errorDescription): boolean
     {
         try {
             $socketConnection = fsockopen($ip, $port, $errorCode, $errorDescription, 10);
             fclose($socketConnection);
             return true;
-        } catch ( \Throwable $th) {
+        } catch (\Throwable $th) {
             return false;
         }
     }
-
-    /**
-     * https://openinternet.io/tor/tor-exit-list.txt
-     */
 }
+
